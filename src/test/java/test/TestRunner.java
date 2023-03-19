1833524a59ebf9d296d922db0cee20593deda1d6
Mokhru <mokhru.niezmamadova@tekschool.us>
2023-03-19 14:29:01 -0400

    Updated code

--- src/test/java/test/TestRunner.java
+++ src/test/java/test/TestRunner.java
@@ -2,12 +2,12 @@ package test;
 
 import com.intuit.karate.junit5.Karate;
 
-public class RetailTestRunner {
+public class TestRunner {
 	
-@Karate.Test
-	
+	@Karate.Test
 	public Karate runTest() {
 		return Karate.run("classpath:features").tags("@completeTest").outputCucumberJson(true);
+		
 	}
 
 }
